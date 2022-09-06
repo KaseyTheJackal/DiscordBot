4967da5593c4afd16c3dcffef76b82b5e49a3a15
Kasey <kaseythejackal@gmail.com>
2022-09-06 14:41:49 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,8 +28,7 @@ else:
     @bot.event
     async def on_ready():
         print(f"We have logged in as {bot.user}")
-
-    # Purge command
+    
     @bot.slash_command()
     @commands.has_permissions(manage_messages=True)
     async def purge(ctx, number: discord.Option(int)):
@@ -38,52 +37,43 @@ else:
                     await ctx.send("Cleared by <@{.author.id}>. There were **{}** messages cleared.".format(ctx, str(number)))
                     await ctx.respond("Successfully cleared messages!")
             if number > 100:
-                await ctx.respond("You cannot purge more than 100 messages at once")            
+                await ctx.respond("You cannot purge more than 100 messages at once")
 
-    # Runs if someone who doesn't have permission attempts to run the command
-    @purge.error
-    async def purge_error(ctx, error):
-        if isinstance(error, MissingPermissions):
-            await ctx.respond("You're not allowed to run this command!")
-    # End of purge command
-
-    # Kick command
     @bot.slash_command()
     @commands.has_permissions(kick_members=True)
     async def kick(ctx):
         await ctx.respond("Not implemented, never will be. Stop being lazy and use Discord's built in kick feature.")
 
-    # Runs if someone who doesn't have permission attempts to run the command
-    @kick.error
-    async def kick_error(ctx, error):
-        if isinstance(error, MissingPermissions):
-            await ctx.respond("You're not allowed to run this command!")
-    # End of kick command
-
-    # Ban command
     @bot.slash_command()
     @commands.has_permissions(ban_members=True)
     async def ban(ctx):
         await ctx.respond("Not implemented, never will be. Stop being lazy and use Discord's built in ban feature.")
 
-    # Runs if someone who doesn't have permission attempts to run the command
-    @ban.error
-    async def ban_error(ctx, error):
-        if isinstance(error, MissingPermissions):
-            await ctx.respond("You're not allowed to run this command!")
-    # End of ban command
-
-    # Mute command
     @bot.slash_command()
     @commands.has_permissions(moderate_members=True)
     async def mute(ctx):
         await ctx.respond("Not implemented, never will be. Stop being lazy and use Discord's built in timeout feature.")
 
-    # Runs if someone who doesn't have permission attempts to run the command
+    # Error handling
+    @purge.error
+    async def purge_error(ctx, error):
+        if isinstance(error, MissingPermissions):
+            await ctx.respond("You're not allowed to run this command!")
+
     @mute.error
     async def mute_error(ctx, error):
         if isinstance(error, MissingPermissions):
             await ctx.respond("You're not allowed to run this command!")
-    # End of mute command
+        # End of mute command
+
+    @ban.error
+    async def ban_error(ctx, error):
+        if isinstance(error, MissingPermissions):
+            await ctx.respond("You're not allowed to run this command!")
+
+    @kick.error
+    async def kick_error(ctx, error):
+        if isinstance(error, MissingPermissions):
+            await ctx.respond("You're not allowed to run this command!")
 
     bot.run(decrypted_key)
